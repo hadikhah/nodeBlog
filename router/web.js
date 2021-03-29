c738283b70fc31e3574a214e6b29412550639865
Mohammad Ali Haidkhah <hadikhahe@gmail.com>
2021-03-30 01:43:12 +0430

    login route added

--- router/web.js
+++ router/web.js
@@ -4,6 +4,9 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     res.render('index', { pageTitle: "وبلاگ" })
 })
+router.get('/Login', (req, res) => {
+    res.render('login', { pageTitle: "ورود" })
+})
 
 
 module.exports = router
