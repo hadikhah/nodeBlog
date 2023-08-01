b82a39e9245217198273887df667c795a27cc546
hadikhah <hadikhahe@gmail.com>
2023-08-01 19:08:16 +0330

    adds user model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    first_name: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    last_name: {
+        type: String,
+        require: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 4,
+        maxlength: 255
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User
