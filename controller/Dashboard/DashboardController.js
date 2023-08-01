1d79d0f1c5a67b092d6af48d74124a78d80a12dc
hadikhah <hadikhahe@gmail.com>
2023-08-01 19:07:53 +0330

    change dashboard views directories

--- controller/Dashboard/DashboardController.js
+++ controller/Dashboard/DashboardController.js
@@ -1,9 +1,10 @@
 class DashboardController {
     showDashboardVuePage(req, res) {
-        res.render('vue', { pageTitle: "ورود", layout: "layouts/empty" })
+        // res.render('vue', { pageTitle: "ورود", layout: "layouts/empty" })
+        res.send("vue")
     }
     showDahsboardPage(req, res) {
-        res.render('dashboard', { pageTitle: "ورود", layout: "layouts/dashboard" })
+        res.render('dashboard/dashboard', { pageTitle: "ورود", layout: "layouts/dashboard" })
     }
 }
 module.exports = DashboardController
