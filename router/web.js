3547db73bd8da4ca9556f46ca3e8b64e1d4025d7
hadikhah <hadikhahe@gmail.com>
2023-08-05 14:05:36 +0330

    add new routes

--- router/web.js
+++ router/web.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const LogoutController = require('../controller/Auth/LogoutController');
+const auth = require('../middleware/auth');
+const guest = require('../middleware/guest');
 // const RegisterValidation = require('../validation/RegisterValidation');
 const {
     LoginController, RegisterController, ResetPasswordController,
@@ -10,16 +13,21 @@ const {
 //home page
 router.get('/', MainController.showIndexPage)
 //auth pages
-router.get('/login', LoginController.showLoginForm)
-router.get('/register', RegisterController.showRegisterForm)
+router.get('/login', guest, LoginController.showLoginForm)
+router.post('/login', guest, LoginController.handleLoginForm)
+
+router.get('/register', guest, RegisterController.showRegisterForm)
+router.post('/register', guest, RegisterValidation, RegisterController.Register)
+
+router.post('/logout', auth, LogoutController.HandleLogout)
+
 router.get('/password/reset', ResetPasswordController.showEmailRequestForm)
 //dashboard page
-router.get('/dashboard', DashboardController.showDahsboardPage)
+router.get('/dashboard', auth, DashboardController.showDahsboardPage)
 router.get("/dashboard/vue/.*/", DashboardController.showDashboardVuePage)
 router.get(/.*/, DashboardController.showDashboardVuePage)
 
 
-router.post('/register', RegisterValidation, RegisterController.Register)
 
 // 404 error page
 router.use((req, res) => {
