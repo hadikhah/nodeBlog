6aab279db0952b2720467680dd38ee1bb5b16bda
hadikhah <hadikhahe@gmail.com>
2023-09-19 15:00:19 +0330

    add type validation for thumbnail

--- validation/PostValidation.js
+++ validation/PostValidation.js
@@ -3,11 +3,21 @@ const yup = require('yup');
 const { Validator } = require('./Validator');
 const Status = require('../models/Status');
 
+const validFileExtensions = { image: ['jpg', 'png', 'jpeg'] };
+
+function isValidFileType(fileName, fileType) {
+    console.log("fileName", fileName)
+    return fileName && validFileExtensions[fileType].indexOf(fileName.split('.').pop()) > -1;
+}
+
 const NewPostFormValidationSchema = (statusListIds) => yup.object().shape({
 
     title: yup.string().required().min(4).max(300),
     status: yup.mixed().oneOf(statusListIds, "status is not valid"),
     body: yup.string().required(),
+    thumbnail: yup.mixed().required()
+        .test("is-valid-type", "Not a valid image type",
+            value => isValidFileType(value && typeof value === "string" ? value.toLowerCase() : value.name?.toLowerCase(), "image"))
 
 });
 
