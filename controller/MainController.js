9352020a86c05891ac13be85dd17070c5177c3dd
hadikhah <hadikhahe@gmail.com>
2023-09-26 13:03:23 +0330

    add truncate to shorten the post brief if its long

--- controller/MainController.js
+++ controller/MainController.js
@@ -1,4 +1,5 @@
 const Status = require("../models/Status")
+const { truncate } = require('../utils/helpers');
 
 /**
  * renders the main page
@@ -12,9 +13,6 @@ exports.showIndexPage = async (req, res) => {
     // gets 8 of latest posts with public status
     const publishedPosts = await Status.findOne({ key: "public" }).populate({ path: "posts", options: { sort: { 'createdAt': -1 }, limit: 8 }, }).lean();
 
-    console.log(publishedPosts)
-
-
-    return res.render('blog/index', { pageTitle: "Blog | Home", layout: "layouts/main", publishedPosts })
+    return res.render('blog/index', { pageTitle: "Blog | Home", layout: "layouts/main", publishedPosts, truncate })
 
 }
