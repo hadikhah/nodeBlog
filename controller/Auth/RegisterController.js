c3c26b56a528c585493a21ce77028868171f5eed
hadikhah <hadikhahe@gmail.com>
2023-10-02 22:27:29 +0330

    add send verification email to register controller

--- controller/Auth/RegisterController.js
+++ controller/Auth/RegisterController.js
@@ -1,8 +1,10 @@
 const bcrypt = require('bcryptjs');
 const { verify } = require('hcaptcha');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/User');
 const { setFormSuccessMessage, setPreviousFormErrors } = require('../../validation/Validator');
+const { renderTemplateEjs, sendMailHTML } = require('../../utils/mail');
 
 /**
  * renders the sign up page
@@ -20,7 +22,6 @@ exports.showRegisterForm = (req, res, next) => {
 }
 
 
-
 /**
  * renders the sign up page
  *
@@ -29,7 +30,7 @@ exports.showRegisterForm = (req, res, next) => {
  * @return {*} 
  * @memberof RegisterController
  */
-exports.Register = (req, res) => {
+exports.Register = (req, res, next) => {
 
     bcrypt.hash(req.body.password, 10, (err, hash) => {
 
@@ -37,17 +38,24 @@ exports.Register = (req, res) => {
 
         const user = new User(req.body);
 
-        user.save().then(() => {
+        user.save().then(async (result) => {
 
-            setFormSuccessMessage(req, "Sign up was successful")
+            await sendUserVerificationEmail(result)
 
-            return res.redirect("/")
+            setFormSuccessMessage(req, "Sign up was successful. validation email has sent")
+
+            req.logIn(user, err => {
+                if (err) return res.redirect("/500")
+
+                return res.redirect("/")
+            })
+
 
         }).catch(err => {
 
             console.log("hello this is error", err)
 
-            redirect("/404")
+            res.redirect("/404")
         });
     })
 
@@ -81,5 +89,50 @@ exports.handleCaptcha = (req, res, next) => {
             }
         })
         .catch(console.error);
+
+}
+
+/**
+ * creates email verification url 
+ *
+ * @param {*} userId
+ * @return {*} 
+ */
+const createUserVerificationUrl = async (userId) => {
+
+    const token = jwt.sign({
+
+        // exp: Math.floor(Date.now() / 1000) + (60 * 5),// 5 min
+        data: {userId}
+
+    },
+        process.env.JWT_SECRET,
+        { expiresIn: 60 * 5 }
+    )
+
+
+    return `${process.env.SITE_URL}/verify_email?token=${token}`;
+}
+
+/**
+ * sends verification email to given user
+ *
+ * @param {*} user
+ * @return {*} 
+ */
+const sendUserVerificationEmail = async (user) => {
+
+    try {
+
+        const verificationUrl = await createUserVerificationUrl(user._id)
+
+        const html = await renderTemplateEjs("verifyEmail.ejs", { verificationUrl, user })
+
+        await sendMailHTML(user.email, "EMAIL VERIFICATION", html)
+
+    } catch (error) {
+
+        throw new Error(error)
+    }
 
 }
