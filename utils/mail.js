584ab25918bd328e96bcd075b1b4128e790a7110
hadikhah <hadikhahe@gmail.com>
2023-10-02 22:25:06 +0330

    add mail utils

---
+++ utils/mail.js
@@ -0,0 +1,99 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+
+const makePath = require("./path");
+
+
+/** 
+ *  email schema
+ * @type {*}
+ */
+const transporter = nodemailer.createTransport({
+	host: process.env.MAIL_HOST,
+	port: process.env.MAIL_PORT,
+	secure: process.env.MAIL_SECURE == "true" ? true : false,
+	auth: {
+		user: process.env.MAIL_USERNAME,
+		pass: process.env.MAIL_PASSWORD,
+	},
+});
+
+/**
+ * send email using html body format
+ *
+ * @param {*} receivers
+ * @param {*} subject
+ * @param {*} htmlBody
+ * @return {*} 
+ */
+exports.sendMailHTML = async (receivers, subject, htmlBody) => {
+
+	try {
+
+		const info = await transporter.sendMail({
+			from: process.env.MAIL_FROM, // sender address
+			to: receivers, // list of receivers
+			subject: subject, // Subject line
+			html: htmlBody, // html body
+		});
+
+		return info
+
+	} catch (error) {
+
+		throw new Error(error);
+	}
+
+}
+
+/**
+ * send email using text only format
+ *
+ * @param {*} receivers
+ * @param {*} subject
+ * @param {*} textBody
+ * @return {*} 
+ */
+exports.sendMailTEXT = async (receivers, subject, textBody) => {
+
+	try {
+
+		const info = await transporter.sendMail({
+			from: process.env.MAIL_FROM, // sender address
+			to: receivers, // list of receivers
+			subject: subject, // Subject line
+			text: textBody, // plain text body
+		});
+
+		return info
+
+	} catch (error) {
+
+		throw new Error(error);
+	}
+}
+
+
+/**
+ * renders ejs file with given data and returns html string
+ *
+ * @param {*} template
+ * @param {*} data
+ * @return {*} 
+ */
+exports.renderTemplateEjs = async (template, data = {}) => {
+	try {
+
+		const path = makePath(["views", "mails", template])
+
+		const html = await ejs.renderFile(path, data)
+
+		return html
+
+	} catch (error) {
+
+		throw new Error(error)
+	}
+
+
+}
