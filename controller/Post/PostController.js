7d7e6715b7bd3fec4c9d8abf5904a0443e6e397e
hadikhah <hadikhahe@gmail.com>
2023-10-19 15:58:00 +0330

    add show all articles

--- controller/Post/PostController.js
+++ controller/Post/PostController.js
@@ -1,4 +1,50 @@
-const Post = require("../../models/Post")
+const Post = require("../../models/Post");
+const Status = require("../../models/Status");
+const { truncate } = require('../../utils/helpers');
+
+
+/**
+ * returns all latest public posts page with search
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @return {*} 
+ */
+exports.allPosts = async (req, res, next) => {
+
+	const perPage = parseInt(req.query.perPage ?? 10);
+
+	const page = parseInt(req.query.page ?? 1);
+
+	try {
+		const publicStatus = await Status.findOne({ key: "public" }).exec()
+
+		let find = { status: publicStatus._id }
+
+		if (req.query?.search)
+			find = { ...find, $text: { $search: req.query?.search } }
+
+		const posts = await Post.find(find)
+			.skip(perPage * (page - 1))
+			.limit(perPage)
+			.sort({
+				createdAt: 'desc'
+			}).populate("user").exec();
+
+
+		const allPostsCount = await Post.countDocuments(find)
+
+		const totalPages = allPostsCount % perPage ? (Math.floor((allPostsCount / perPage) + 1)) : (Math.floor(allPostsCount / perPage))
+
+		const { page: _, ...query } = req.query
+
+		return res.render("blog/posts", { pageTitle: "all posts", layout: "layouts/main", posts, currentPage: page, perPage, allPostsCount, totalPages, query, truncate })
+
+	} catch (err) {
+		next(err)
+	}
+}
 
 /**
  * renders the post by given id
--- router/post.js
+++ router/post.js
@@ -5,7 +5,13 @@ const PostController = require('../controller/Post/PostController');
 /** 
  * @route
  * @get
- * @desc dashboard post edit page 
+ * @desc show all latest posts with search and patination 
+ */
+router.get('/posts', PostController.allPosts)
+/** 
+ * @route
+ * @get
+ * @desc show the post by given uuid 
  */
 router.get('/post/:id', PostController.showPost)
 
